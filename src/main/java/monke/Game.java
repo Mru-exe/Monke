d2cca32824835653df88439e997205c2545d1c95
kindlma7 <kindlma7@cvut.cz>
2025-04-12 20:03:29 +0200

    Added TODOs to Game.java

--- src/main/java/monke/Game.java
+++ src/main/java/monke/Game.java
@@ -8,12 +8,16 @@ public class Game {
     public static void main(String[] args) {
         Level currentLevel = LevelLoader.loadLevel("defaultLevel.json"); //TODO
 
+        //GameWindow window = new GameWindow("Monke Game");
         startGameLoop(currentLevel);
     }
 
     private static void startGameLoop(Level level) {
+        //TODO: open main game window
+
         level.spawnPlayer(new Player(0, 0, 0, 50, 50));
 
         //TODO loop
+        
     }
 }
