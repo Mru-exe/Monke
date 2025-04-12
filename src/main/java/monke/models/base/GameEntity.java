1bc98511544ce037a0513a8451631f7d7d6ed836
kindlma7 <kindlma7@cvut.cz>
2025-04-12 19:45:30 +0200

    updated base model classes

--- src/main/java/monke/models/base/GameEntity.java
+++ src/main/java/monke/models/base/GameEntity.java
@@ -1,4 +1,29 @@
-package monke.models.common;
+package monke.models.base;
 
-public abstract class GameEntity {
+import monke.models.common.BoundingBox;
+import monke.models.common.Collidable;
+
+public abstract class GameEntity extends GameObject implements Collidable {
+    private BoundingBox boundary = null;
+
+    public GameEntity(int x, int y, float rotation) {
+        super(x, y, rotation);
+    }
+    public GameEntity(int x, int y, float rotation, BoundingBox boundary) {
+        super(x, y, rotation);
+        this.boundary = boundary;
+    }
+
+    public void setBounds(BoundingBox boundary) {
+        this.boundary = boundary;
+    }
+
+    public BoundingBox getBounds(){
+        return this.boundary;
+    };
+
+    @Override
+    public void onCollision(Collidable other) {
+        //TODO
+    }
 }
--- src/main/java/monke/models/base/GameObject.java
+++ src/main/java/monke/models/base/GameObject.java
@@ -1,4 +1,48 @@
-package monke.models.common;
+package monke.models.base;
+import java.util.UUID;
 
 public abstract class GameObject {
+    protected final UUID id;
+
+    private int x;
+    private int y;
+    protected float rotation = 0.0f; // Rotation in degrees
+
+    public GameObject(int x, int y, float rotation) {
+        this.id = UUID.randomUUID();
+
+        this.x = x;
+        this.y = y;
+        this.rotation = rotation;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getDebugString() {
+        return this.getClass().getSimpleName() + "[" + this.id + "]";
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public float getRotation() {
+        return rotation;
+    }
+
+    public void setCoords(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    public void setCoords(int x, int y, float rotation) {
+        this.rotation = rotation;
+        this.x = x;
+        this.y = y;
+    }
 }
