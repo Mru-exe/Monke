e374473709cc7b5bef16cdf46ad95e2af9392978
kindlma7 <kindlma7@cvut.cz>
2025-04-12 19:56:01 +0200

    Modified LevelLoader to be static

--- src/main/java/monke/controllers/LevelLoader.java
+++ src/main/java/monke/controllers/LevelLoader.java
@@ -1,4 +1,12 @@
 package monke.controllers;
 
+import monke.models.Level;
+
 public class LevelLoader {
+
+    public static Level loadLevel(String levelFilePath) {
+        //TODO: deserialize 
+
+        return new Level("", 0, 0);
+    }
 }
