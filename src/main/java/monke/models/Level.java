aba952350fcd2f64a2ee6d87dbf3e33eb8d23dac
kindlma7 <kindlma7@cvut.cz>
2025-04-12 19:47:16 +0200

    Updated Level class

--- src/main/java/monke/models/Level.java
+++ src/main/java/monke/models/Level.java
@@ -1,4 +1,52 @@
 package monke.models;
 
+import monke.models.entities.Monkey;
+import monke.models.entities.Player; 
+
+import java.util.UUID;
+
 public class Level {
+    private String name;
+    private UUID levelId;
+
+    // private int guiScale;
+    // private int width;
+    // private int height;
+    
+    private int spawnX;
+    private int spawnY;
+
+    private Player player;
+    private Monkey monkey;
+    private Goal goal;
+
+    private Platform[] platforms;
+
+
+    public Level(String name, int spawnX, int spawnY) {
+        this.name = name;
+        this.spawnX = spawnX;
+        this.spawnY = spawnY;
+        this.levelId = UUID.randomUUID();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public UUID getId() {
+        return levelId;
+    }
+
+    public void setMonkey(Monkey monkey) {
+        this.monkey = monkey;
+    }
+
+    public void setPlatforms(Platform[] platforms) {
+        this.platforms = platforms;
+    }
+
+    public void setGoal(Goal goal) {
+        this.goal = goal;
+    }
 }
