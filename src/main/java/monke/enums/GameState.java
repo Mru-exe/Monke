1e06a5c1197e1593f9a7ae90f7938eca9eca8b69
kindlma7 <kindlma7@cvut.cz>
2025-04-28 20:03:39 +0200

    Created State Enumes

---
+++ src/main/java/monke/enums/GameState.java
@@ -0,0 +1,12 @@
+package monke.enums;
+
+public enum GameState {
+    MAIN_MENU,
+    GAME,
+    PAUSED,
+    GAME_OVER,
+    SETTINGS,
+    CREDITS,
+    LEVEL_SELECT,
+    LOADING;
+}
---
+++ src/main/java/monke/enums/PlayerState.java
@@ -0,0 +1,9 @@
+package monke.enums;
+
+public enum PlayerState {
+    WALKING,
+    IDLE,
+    JUMPING,
+    CLIMBING,
+    FALLING;
+}
