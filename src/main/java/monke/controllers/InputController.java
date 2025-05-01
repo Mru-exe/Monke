2fa0afbd6839ec4894483df4c57401eddd81c84e
kindlma7 <kindlma7@cvut.cz>
2025-05-01 19:41:12 +0200

    Renamed to InputController and added keyBindings map. Refactored to be JavaFX independent

--- src/main/java/monke/controllers/InputController.java
+++ src/main/java/monke/controllers/InputController.java
@@ -1,14 +1,48 @@
 package monke.controllers;
 
 
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import monke.enums.Command;
+import monke.enums.GameEvent;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+
 /**
  * InputHandler is responsible for handling user input events.
  * (TBD) Raises event on key presses and mouse clicks
  */
-public class InputHandler {
-    //TODO, probably observer pattern
+public abstract class InputController {
+    private static Logger logger = Logger.getLogger(InputController.class.getName());
 
-    public void handleInput() {
-        // Handle input events
+    public static final ConcurrentHashMap<String, Command> keyBindings = new ConcurrentHashMap<>();
+
+    static {
+        keyBindings.put("A", Command.PLAYER_LEFT);
+        keyBindings.put("LEFT", Command.PLAYER_LEFT);
+        keyBindings.put("D", Command.PLAYER_RIGHT);
+        keyBindings.put("RIGHT", Command.PLAYER_RIGHT);
+        keyBindings.put("SPACE", Command.PLAYER_JUMP);
+        keyBindings.put("UP", Command.PLAYER_JUMP);
+        keyBindings.put("ESCAPE", Command.GAME_QUIT);
+    }
+
+    public static Command parse(String keyCode, boolean isReleased){
+        logger.finest("Parsing keyCode: " + keyCode + " isReleased: " + isReleased);
+        if(isReleased &&
+                (keyBindings.get(keyCode) == Command.PLAYER_LEFT ||
+                keyBindings.get(keyCode) == Command.PLAYER_RIGHT)) {
+            return Command.PLAYER_STOP;
+        } else if (!isReleased && keyBindings.containsKey(keyCode)) {
+            return keyBindings.get(keyCode);
+        } else {
+            return Command.UNBOUND;
+        }
+    }
+
+    public static void bind(String key, Command command) {
+        keyBindings.put(key, command);
     }
 }
+
