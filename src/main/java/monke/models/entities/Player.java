8d8a5678b863159e09f9bbd08dc9048b8bba94fe
kindlma7 <kindlma7@cvut.cz>
2025-05-08 12:41:24 +0200

    Implement onCollision method in Player class and removed redundant properties

--- src/main/java/monke/models/entities/Player.java
+++ src/main/java/monke/models/entities/Player.java
@@ -3,25 +3,19 @@ package monke.models.entities;
 import monke.enums.PlayerState;
 import monke.models.base.GameEntity;
 import monke.models.common.BoundingBox;
+import monke.models.common.Collidable;
 
 /**
  * Represents a player in the game.
  */
 public class Player extends GameEntity{
 
-    private PlayerState state;
-    private final float jumpStrength = 8;
-    private final float moveSpeed = 5;
-
     public Player(int x, int y, int boundaryWidth, int boundaryHeight) {
         super(x, y, new BoundingBox(x, y, boundaryWidth, boundaryHeight));
     }
 
-    public float getJumpStrength() {
-        return jumpStrength;
-    }
-
-    public float getMoveSpeed() {
-        return moveSpeed;
+    @Override
+    public void onCollision(Collidable other) {
+        //TODO collision
     }
 }
