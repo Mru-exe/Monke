b04272e28a249500d732c7244e6b1899e5e72b4a
kindlma7 <kindlma7@cvut.cz>
2025-05-09 14:08:11 +0200

    Removed drafted methods, imported BoundingBox

--- src/main/java/monke/models/entities/Monkey.java
+++ src/main/java/monke/models/entities/Monkey.java
@@ -1,20 +1,16 @@
 package monke.models.entities;
 
 import monke.models.base.GameEntity;
+import monke.models.common.BoundingBox;
 
 /**
  * Represents a monkey in the game.
  * Basically just origin coords for barrel spawns.
+ * TODO
  */
 public class Monkey extends GameEntity{
     public Monkey(double x, double y) {
         super(x, y, new BoundingBox(0, 0, 0, 0));
     }
 
-    @Override
-    public void update() {
-        //TODO: Throw barrel on cooldown
-        //TODO: Animate monkey
-        return;
-    }
 }
