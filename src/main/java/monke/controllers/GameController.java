d347a485d32979fb1c452b404b5b79efab111fdd
kindlma7 <kindlma7@cvut.cz>
2025-05-09 11:08:27 +0200

    Refactor GameController to improve collision handling and update logic; adjusted player movement forces and damping.

--- src/main/java/monke/controllers/GameController.java
+++ src/main/java/monke/controllers/GameController.java
@@ -5,9 +5,9 @@ import javafx.scene.Scene;
 import monke.enums.Command;
 import monke.enums.GameEvent;
 import monke.models.GameLevel;
-import monke.models.Platform;
 import monke.models.base.GameEntity;
 import monke.models.base.GameObject;
+import monke.models.common.Collidable;
 import monke.models.common.Updatable;
 import monke.models.entities.Player;
 import monke.utils.EventBus;
@@ -16,23 +16,23 @@ import monke.utils.InputHandler;
 import monke.utils.SpriteFactory;
 import monke.views.GameView;
 
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.logging.Logger;
 
 public class GameController {
     private static final Logger logger = Logger.getLogger(GameController.class.getName());
-    private final SpriteFactory spriteFactory = new SpriteFactory(true);
-
-    private final Set<GameObject> activeSprites = new HashSet<>();
+    private final SpriteFactory sf = new SpriteFactory(true);
 
     private final GameLoop logicThread = new GameLoop(){
         @Override
-        protected void process() {
-            for (GameObject go : activeSprites) {
-                if (go instanceof Updatable) {
-                    ((Updatable) go).update();
+        protected void process(double dt) {
+            for (Updatable u : level.getUpdatable()) {
+                u.update(dt);
+            }
+            for (Collidable c : level.getCollidable()) {
+                for (Collidable other : level.getCollidable()) {
+                    if (c.hashCode() != other.hashCode() && c.getBounds().intersects(other.getBounds())) {
+                        c.onCollision(other);
+                    }
                 }
             }
         }
@@ -44,28 +44,19 @@ public class GameController {
 
     public GameController(GameLevel level) {
         this.view = new GameView(this);
-//        this.level = level;
-        this.level = new GameLevel("TestLevel", 0, 0);
-
-        Player player = new Player(0,100,10,10);
-        spriteFactory.applySpriteToModel(player);
-
-        Platform ground = new Platform(0, 100, 190, 10);
-        spriteFactory.applySpriteToModel(ground);
-
-        this.player = player;
-        this.activeSprites.add(player);
-        this.activeSprites.add(ground);
-        this.pushSpritesToView(activeSprites);
-
         logger.finer("GameController initialized");
-        view.startRenderingLoop(activeSprites);
-        this.logicThread.start();
-    }
 
-    private void pushSpritesToView(Collection<GameObject> sprites){
-        logger.finer("Pushing sprites to view");
-        this.view.addGameObjects(sprites);
+        this.level = level;
+        for (GameObject go : level.getGameObjects()) {
+            sf.applySpriteToModel(go);
+        }
+        this.player = level.getPlayer();
+
+
+        view.pushSprites(level.getGameObjects());
+
+        view.startRenderingThread();
+        this.logicThread.start();
     }
 
     public void handleInput(String key, boolean release){
@@ -74,10 +65,14 @@ public class GameController {
         Command cmd = InputHandler.parse(key, release);
 
         switch (cmd){
-            case Command.PLAYER_LEFT -> player.applyForceX(-5);
-            case Command.PLAYER_RIGHT -> player.applyForceX(5);
-            case Command.PLAYER_JUMP -> player.applyForceY(-26.2f* GameEntity.gravityStrength);
-            case Command.PLAYER_STOP -> player.applyFriction(0.9f); //Percentual friction (0.9 = loses 10% velocity each frame)
+            case Command.PLAYER_LEFT -> player.applyForceX(-15);
+            case Command.PLAYER_RIGHT -> player.applyForceX(15);
+            case Command.PLAYER_JUMP -> {
+                if(Math.abs(player.getVelY()) <= 0) {
+                    player.applyForceY(-4.5*GameEntity.gravityStrength);
+                }
+            }
+            case Command.PLAYER_STOP -> player.setDamping(0.7d); //Percentual friction (0.8 = loses 20% velocity each frame)
             case Command.GAME_QUIT -> {
                 logger.finer("Game quit");
                 logicThread.stop();
@@ -89,14 +84,4 @@ public class GameController {
     public Scene getView() {
         return this.view;
     }
-
-//    public void pauseGame(){
-//        logger.finer("Game paused");
-//        EventBus.publish(GameEvent.PAUSE_GAME);
-//    }
-//
-//    public void resumeGame(){
-//        logger.finer("Game resumed");
-//        EventBus.publish(GameEvent.RESUME_GAME);
-//    }
 }
