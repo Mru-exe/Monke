ed8c17ce9626a8a39d4e41dc23cee33db8b8c48d
kindlma7 <kindlma7@cvut.cz>
2025-05-11 17:34:37 +0200

    Add barrel spawning functionality to Monkey class

--- src/main/java/monke/models/entities/Monkey.java
+++ src/main/java/monke/models/entities/Monkey.java
@@ -1,7 +1,13 @@
 package monke.models.entities;
 
+import monke.enums.SpriteImage;
 import monke.models.base.GameEntity;
 import monke.models.common.BoundingBox;
+import monke.utils.SpriteFactory;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+import java.util.logging.Logger;
 
 /**
  * Represents a monkey in the game.
@@ -9,8 +15,49 @@ import monke.models.common.BoundingBox;
  * TODO
  */
 public class Monkey extends GameEntity{
-    public Monkey(double x, double y) {
-        super(x, y, new BoundingBox(0, 0, 0, 0));
+    private static final Logger logger = Logger.getLogger(Monkey.class.getName());
+    private Set<Barrel> barrels = new CopyOnWriteArraySet<>();
+
+    private SpriteFactory sf = new SpriteFactory();
+
+    private double cooldown = 10d;
+
+    public Monkey(double x, double y, Set<Barrel> barrelSet) {
+        super(x, y, new BoundingBox(x, y, 64, 64));
+        this.img = SpriteImage.MONKEY;
+        this.barrels = barrelSet;
     }
 
+    private void spawnBarrel() {
+        Barrel barrel = new Barrel((int) x-64, (int) y);
+        sf.applySpriteToModel(barrel);
+        barrels.add(barrel);
+    }
+
+    @Override
+    public void update(double dt) {
+        super.update(dt);
+        cooldown -= dt;
+        if(cooldown <= 0){
+            spawnBarrel();
+            cooldown = 10d;
+        }
+    }
+
+    @Override
+    public void resolveHorizontalCollision(double dx, double overlapX) {
+        double shiftX = dx > 0 ? overlapX : -overlapX;
+        this.setCoords(this.getX() + shiftX, this.getY());
+        this.applyForceX(0);
+    }
+
+    @Override
+    public void resolveVerticalCollision(double dy, double overlapY) {
+        if(dy > 0){
+            this.applyForceY(gravityStrength);
+        } else {
+            this.setCoords(this.getX(), this.getY() - overlapY);
+            this.applyForceY(0);
+        }
+    }
 }
