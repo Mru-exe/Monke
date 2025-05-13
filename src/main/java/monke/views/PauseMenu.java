15db581593078efeac84be4cc3e4d11902aa4112
kindlma7 <kindlma7@cvut.cz>
2025-05-13 21:46:26 +0200

    Removed unused classes

--- src/main/java/monke/views/PauseMenu.java
+++
@@ -1,8 +0,0 @@
-package monke.views;
-
-/**
- * PauseMenu is responsible for displaying the pause menu.
- * It allows the player to resume the game or quit to the main menu.
- */
-public class PauseMenu {
-}
