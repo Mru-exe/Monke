67e7c3765c9283453bdc3a1a3636b0be5794b3f2
kindlma7 <kindlma7@cvut.cz>
2025-05-14 12:01:20 +0200

    Removed unused commands

--- src/main/java/monke/enums/Command.java
+++ src/main/java/monke/enums/Command.java
@@ -5,13 +5,7 @@ public enum Command {
     //PLAYER
     PLAYER_LEFT,
     PLAYER_RIGHT,
-    PLAYER_STOP,
     PLAYER_JUMP,
-    PLAYER_ATTACK,
-    PLAYER_DROP,
     //GAME
-    TOGGLE_PAUSE,
-    GAME_RESUME,
-    GAME_RESTART,
-    GAME_QUIT,
+    TOGGLE_PAUSE
 }
