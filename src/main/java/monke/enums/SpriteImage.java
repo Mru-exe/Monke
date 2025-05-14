d36b6fa126482f71fa22716069a21f98a4e04be0
kindlma7 <kindlma7@cvut.cz>
2025-05-14 15:19:27 +0200

    Enhance SpriteImage enum with dimensions for sprites

--- src/main/java/monke/enums/SpriteImage.java
+++ src/main/java/monke/enums/SpriteImage.java
@@ -1,17 +1,30 @@
 package monke.enums;
 
 public enum SpriteImage {
-    NOTEXTURE("/sprites/notexture.png"),
-    PLAYER("/sprites/player/player-still.png"),
-    BARREL("/sprites/barrel2.png");
-//    ITEM("item/item.png"),
-//    BACKGROUND("background/background.png"),
-//    ENEMY("enemy/enemy.png");
+    NOTEXTURE("/sprites/notexture.png", 16, 16),
+    MONKEY("/sprites/monkey.png", 64, 64),
+    BARREL("/sprites/barrel.png", 48, 48),
+    PLATFORM("/sprites/platform.png", 24, 24),
+    KEY("/sprites/key.png", 22, 22),
+    GOAL("/sprites/door.png", 64, 64),
+    PLAYER("/sprites/player.png", 32, 32);
 
     private final String path;
+    private final int height;
+    private final int width;
 
-    SpriteImage(String path) {
+    SpriteImage(String path, int height, int width) {
         this.path = path;
+        this.height = height;
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getWidth() {
+        return width;
     }
 
     public String getPath() {
